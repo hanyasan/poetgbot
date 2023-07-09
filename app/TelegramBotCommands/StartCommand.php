4d9d8cee847552f99db613b1ad123dd0dc28465f
Травников Никита <n.travnikov@qsoft.ru>
2023-07-09 17:36:54 +0300

    Divine and Help commands added

--- app/TelegramBotCommands/StartCommand.php
+++ app/TelegramBotCommands/StartCommand.php
@@ -1,25 +1,5 @@
 <?php
 
-/**
- * This file is part of the PHP Telegram Bot example-bot package.
- * https://github.com/php-telegram-bot/example-bot/
- *
- * (c) PHP Telegram Bot Team
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-/**
- * Start command
- *
- * Gets executed when a user first starts using the bot.
- *
- * When using deep-linking, the parameter can be accessed by getting the command text.
- *
- * @see https://core.telegram.org/bots#deep-linking
- */
-
 namespace App\TelegramBotCommands;
 
 use Longman\TelegramBot\Commands\SystemCommand;
@@ -46,7 +26,7 @@ class StartCommand extends SystemCommand
     /**
      * @var string
      */
-    protected $version = '1.2.0';
+    protected $version = '1.0';
 
     /**
      * @var bool
@@ -65,7 +45,7 @@ class StartCommand extends SystemCommand
         // $deep_linking_parameter = $this->getMessage()->getText(true);
 
         return $this->replyToChat(
-            'Hi there!' . PHP_EOL .
+            'Fuck off!' . PHP_EOL .
             'Type /help to see all commands!'
         );
     }
